bbd2cd1ae2926b8de253afea583acda84bf79bea
Avidor <avidor98@gmail.com>
2017-03-28 02:13:23 +0300

    Add functionality for inline queries (search via spotify)

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ const spotify = new SpotifyWebApi({
 });
 
 const telegram = new TelegramBot(config.telegramToken, {polling: true});
+
+telegram.on('message', (msg) => {
+  console.log(msg); // todo send a message explaining about bot usage
+}); 
+
+telegram.on('inline_query', async (query) => {
+  const search = await spotify.searchTracks(query.query, {limit: 5});
+
+  let tracks = [];
+  search.body.tracks.items.forEach((track) => {
+    tracks.push({
+      type: 'article',
+      id: track.id,
+      title: `${track.artists[0].name} - ${track.name}`,
+      description: track.album.name,
+      thumb_url: track.album.images[0].url,
+      input_message_content: {
+        message_text: track.external_urls.spotify
+      }
+    });
+  });
+
+  telegram.answerInlineQuery(query.id, tracks);
+});
