db3899bf4cc407c61e0fe3ca5ef987f32bd4c988
Avidor <avidor98@gmail.com>
2017-03-28 03:05:41 +0300

    Add protection against running without config & reply on user message

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
 'use strict';
 
-const [bluebird, SpotifyWebApi, TelegramBot, config] = [
-  require('bluebird'), require('spotify-web-api-node'),
-  require('node-telegram-bot-api'), require('rc')('botify')
+const [bluebird, TelegramBot, SpotifyWebApi, config] = [
+  require('bluebird'), require('node-telegram-bot-api'),
+  require('spotify-web-api-node'), require('rc')('botify')
 ];
 
-const spotify = new SpotifyWebApi({
-  clientId: config.spotify.clientId,
-  clientSecret: config.spotify.clientSecret
-});
-
-const telegram = new TelegramBot(config.telegramToken, {polling: true});
-
-telegram.on('message', (msg) => {
-  console.log(msg); // todo send a message explaining about bot usage
-});
-
-telegram.on('inline_query', async (query) => {
-  if (!query.query) {
-    return;
+(function() {
+  if (!config.config) {
+    return console.error('[spotify-telegram] Unable to start without config');
   }
 
-  try {
-    const search = await spotify.searchTracks(query.query, {limit: config.spotify.limitResults});
+  const telegram = new TelegramBot(config.telegram.token, {polling: true});
+  const spotify = new SpotifyWebApi({
+    clientId: config.spotify.clientId,
+    clientSecret: config.spotify.clientSecret
+  });
 
-    let tracks = [];
-    search.body.tracks.items.forEach((track) => {
-      tracks.push({
-        type: 'article',
-        id: track.id,
-        title: `${track.artists[0].name} - ${track.name}`,
-        description: track.album.name,
-        thumb_url: track.album.images[0].url,
-        input_message_content: {
-          message_text: track.external_urls.spotify
-        }
+  console.log('[spotify-telegram] Started');
+  
+  telegram.on('message', (msg) =>
+    telegram.sendMessage(msg.chat.id, config.telegram.message, {parse_mode: 'markdown'}));
+
+  telegram.on('inline_query', async (query) => {
+    if (!query.query) {
+      return;
+    }
+
+    try {
+      const search = await spotify.searchTracks(query.query, {limit: config.spotify.limitResults});
+
+      let tracks = [];
+      search.body.tracks.items.forEach((track) => {
+        tracks.push({
+          type: 'article',
+          id: track.id,
+          title: `${track.artists[0].name} - ${track.name}`,
+          description: track.album.name,
+          thumb_url: track.album.images[0].url,
+          input_message_content: {
+            message_text: track.external_urls.spotify
+          }
+        });
       });
-    });
 
-    telegram.answerInlineQuery(query.id, tracks);
-  } catch (e) {
-    console.error('[SpotifyWebApi] %s', e);
-  }
-});
+      telegram.answerInlineQuery(query.id, tracks);
+    } catch (e) {
+      console.error('[SpotifyWebApi] %s', e);
+    }
+  });
+})();
